56699812d607ca79eec3a69bfaa8ad4a0544d6b5
giorgishushiashvili <gio_shusha@mail.ru>
2023-08-02 13:45:59 +0400

    Added vary simple UI

---
+++ GuildMaster/Assets/Scripts/Core/UI Display.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+using Unity.VisualScripting;
+
+public class UIDisplay : MonoBehaviour
+{
+    public TMP_Text dayNightClock;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float Time = this.GetComponent<GameTime>().SecondsLeft();
+        if ( Time > 0)
+        {
+            dayNightClock.text = Time.ToString();
+        }
+        else
+        {
+            dayNightClock.text = "Currently is night";
+        }
+    }
+}
